8d16412fc11ceaee4f78d1b7b8c67b9d42ee9be8
hannamhiri <hanna.mhiri@gmail.com>
2025-12-21 21:25:34 +0100

    Rewrite model_evaluation cleanly

--- src/mlProject/components/model_evaluation.py
+++ src/mlProject/components/model_evaluation.py
@@ -6,32 +6,20 @@ import mlflow
 import mlflow.sklearn
 import numpy as np
 import joblib
-<<<<<<< HEAD
-from mlProject.entity.config_entity import ModelEvaluationConfig
-from mlProject.utils.common import save_json
-from pathlib import Path
-from mlProject import logger
-
-
-=======
 from pathlib import Path
 from mlProject.entity.config_entity import ModelEvaluationConfig
 from mlProject.utils.common import save_json
 from mlProject import logger
 
->>>>>>> 73c82e7694744578b42be96fa80f905c120950fd
 class ModelEvaluation:
     def __init__(self, config: ModelEvaluationConfig):
         self.config = config
 
-<<<<<<< HEAD
-=======
         # 🔹 Définir les variables d'environnement pour MLflow distant
         os.environ["MLFLOW_TRACKING_URI"] = self.config.mlflow_uri
         os.environ["MLFLOW_TRACKING_USERNAME"] = "hannamhiri"  # ton user DagsHub
         os.environ["MLFLOW_TRACKING_PASSWORD"] = "d818c76624661ed3e44ed5cd15bb08d17cd94c4d"  # token perso
 
->>>>>>> 73c82e7694744578b42be96fa80f905c120950fd
     def eval_metrics(self, actual, pred):
         rmse = np.sqrt(mean_squared_error(actual, pred))
         mae = mean_absolute_error(actual, pred)
@@ -39,62 +27,33 @@ class ModelEvaluation:
         return rmse, mae, r2
 
     def log_into_mlflow(self):
-<<<<<<< HEAD
-
-=======
->>>>>>> 73c82e7694744578b42be96fa80f905c120950fd
         test_data = pd.read_csv(self.config.test_data_path)
         test_x = test_data.drop([self.config.target_column], axis=1)
         test_y = test_data[self.config.target_column]
 
-<<<<<<< HEAD
-        mlflow.set_registry_uri(self.config.mlflow_uri)
-        tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
-
-        # 🔁 Boucle sur tous les modèles sauvegardés
-        for model_file in os.listdir(self.config.model_dir):
-
-=======
         tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
         logger.info(f"Tracking URI: {mlflow.get_tracking_uri()}")
 
         for model_file in os.listdir(self.config.model_dir):
->>>>>>> 73c82e7694744578b42be96fa80f905c120950fd
             if not model_file.endswith(".pkl"):
                 continue
 
             model_name = model_file.replace(".pkl", "")
             model_path = os.path.join(self.config.model_dir, model_file)
-<<<<<<< HEAD
-
-=======
->>>>>>> 73c82e7694744578b42be96fa80f905c120950fd
             logger.info(f"Evaluating model: {model_name}")
 
             model = joblib.load(model_path)
 
             with mlflow.start_run(run_name=model_name):
-<<<<<<< HEAD
-
-=======
->>>>>>> 73c82e7694744578b42be96fa80f905c120950fd
                 predictions = model.predict(test_x)
                 rmse, mae, r2 = self.eval_metrics(test_y, predictions)
 
                 scores = {"rmse": rmse, "mae": mae, "r2": r2}
-<<<<<<< HEAD
-
-=======
->>>>>>> 73c82e7694744578b42be96fa80f905c120950fd
                 save_json(
                     path=Path(os.path.join(self.config.root_dir, f"{model_name}_metrics.json")),
                     data=scores
                 )
 
-<<<<<<< HEAD
-                # 🔹 Log params DU modèle
-=======
->>>>>>> 73c82e7694744578b42be96fa80f905c120950fd
                 if model_name in self.config.all_params:
                     mlflow.log_params(self.config.all_params[model_name])
 
@@ -103,19 +62,6 @@ class ModelEvaluation:
                 mlflow.log_metric("r2", r2)
 
                 if tracking_url_type_store != "file":
-<<<<<<< HEAD
-                    mlflow.sklearn.log_model(
-                        model,
-                        "model",
-                        registered_model_name=f"{model_name}Model"
-                    )
-                else:
-                    mlflow.sklearn.log_model(model, "model")
-
-
-    
-=======
                     mlflow.sklearn.log_model(model, "model", registered_model_name=f"{model_name}Model")
                 else:
                     mlflow.sklearn.log_model(model, "model")
->>>>>>> 73c82e7694744578b42be96fa80f905c120950fd
